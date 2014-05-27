19e7697fde0acc940f5be1e69317e57bd6f0e6c7
samskinnerphd <samskinnerphd@gmail.com>
2014-05-27 09:02:31 -0400

    table of contents

---
+++ table_of_contents.rb
@@ -0,0 +1,22 @@
+whline_width = 75
+puts ('Table of Contents'.rjust(line_width))
+puts
+puts ('Chapter 1'.ljust(15) + 'Getting Started'.center(15) + 'page 1'.rjust(25))
+puts ('Numbers'.center(30) + 'page 9'.rjust(45))
+puts ('Letters'.center(25) + 'page 13'.rjust(40))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
