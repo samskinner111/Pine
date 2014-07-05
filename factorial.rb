b7d1f2b84d1c584838d6656d6593b1a25588e866
samskinnerphd <samskinnerphd@gmail.com>
2014-07-05 09:09:44 -0400

    factorial

---
+++ factorial.rb
@@ -0,0 +1,13 @@
+#this Pine program uses recursion to calculate factorials.
+def factorial num
+  if num < 0
+    return 'You can\'t take the factorial of a negative!'
+  end
+  if num <= 1
+    1
+  else
+    num * factorial(num-1)
+  end
+end
+puts factorial (3)
+puts factorial (30)
