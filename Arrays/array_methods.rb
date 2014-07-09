a8f0f5bdad2a97e1988bba979cee01a424474032
samskinnerphd <samskinnerphd@gmail.com>
2014-07-09 10:00:22 -0400

    array_methods

---
+++ Arrays/array_methods.rb
@@ -0,0 +1,21 @@
+# Methods for adding values at either end of an array, how to print out the values.
+b = ["cat", 2, "fly"]
+b.push ("dog")
+b.unshift (6)
+
+puts b
+puts
+print b
+puts
+
+# Methods for removing values from an array
+b = [6, "cat", 2, "fly", "dog"]
+print b
+puts
+b.pop
+b.shift
+
+puts b
+puts
+print b
+puts
