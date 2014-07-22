8ae2c5c29603cc7743da9c7427d9e1c94d9202f6
samskinnerphd <samskinnerphd@gmail.com>
2014-07-22 09:17:51 -0400

    time

---
+++ time.rb
@@ -0,0 +1,21 @@
+alpha = Array.new + [49325]
+beta = String.new + 'freaky Friday!'
+karma = Time.new
+
+puts "alpha = #{alpha}"
+puts "beta = #{beta}"
+puts karma = "karma = #{karma}"
+puts
+
+time = Time.new
+time2 = time + 120
+
+puts time
+puts time2
+puts
+
+a = Time.local(1952, 6, 26, 7, 30)
+b = (Time.local(1952, 6, 26, 7, 30) - Time.local(1996, 12, 16, 7, 42))
+puts a
+puts b
+puts b/31536000  #my approximate age when Rachel was born
