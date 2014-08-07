3c6d2492f622b157410027acd972cc6bcc8796fa
samskinnerphd <samskinnerphd@gmail.com>
2014-08-07 09:21:35 -0400

    admin

---
+++ admin.rb
@@ -0,0 +1,11 @@
+puts "What's your name?"
+name = gets.chomp
+
+if name == "Peter" || name == "Henry"
+  puts "Welcome back, administrator."
+else
+  puts "You're just a lowly student, good-bye!"
+end
+puts
+
+
