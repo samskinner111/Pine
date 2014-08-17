d13390c98376fc40e00530ff12b435d2b6af463b
samskinnerphd <samskinnerphd@gmail.com>
2014-08-17 09:26:07 -0400

    teams

---
+++ teams.rb
@@ -0,0 +1,7 @@
+
+
+columns = [ "Name", "Average", "Math", "Science", "English" ] 
+rows = [ [ "Alice", 0, 95, 90, 85 ], [ "Bob", 0, 65, 70, 90 ], [ "Carol", 0, 95, 80, 85 ], [ "Dave", 0, 75, 90, 90 ] ]
+
+
+
