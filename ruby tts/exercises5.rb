26a9cfc07a462dc2a0031b5972edf6112126d2a4
samskinnerphd <samskinnerphd@gmail.com>
2014-10-14 10:04:39 -0400

    exercises5

---
+++ ruby tts/exercises5.rb
@@ -0,0 +1,6 @@
+x = {"make"=>"toyota", "model"=>"corolla"}
+
+person = {"sex"=>"female", "hair"=>"brown", "nationality"=>"american"}
+
+person = {"sex"=>"female", "hair"=>"brown", "nationality"=>"american", "first_name"=>"dawn", "last_name"=>"midnight"}
+
