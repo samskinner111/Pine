44e2c69e2ef9207bd432c2beb6378067eae06209
samskinnerphd <samskinnerphd@gmail.com>
2014-11-03 10:14:06 -0500

    states2

---
+++ ruby tts/states2.rb
@@ -0,0 +1,11 @@
+puts "Enter all the states you've been to! Type 'done' when you're finished:"
+states_visited = gets.chomp
+
+un
+if states_visited != 'done'
+	visited = visited.push(states_visited)
+else
+#puts states_visited
+end
+
+puts visited
